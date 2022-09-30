abb09e69bfba25e344e5872cfc245fc619854d18
Sergey <motor-person@yandex.ru>
2022-09-30 15:14:11 +0400

    Сделали задание Task_07

---
+++ Task_07/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+Console.WriteLine("Пожалуйста, ввведите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int res = num %  10;
+Console.WriteLine($"Число {res} является последней цифрой числа {num}");
