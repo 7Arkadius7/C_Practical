3ca08a1cf9f47e241c969cc7b7a0ca89cb177c12
Sergey <motor-person@yandex.ru>
2022-09-30 15:47:47 +0400

    Сделали задание Task_04

---
+++ Task_04/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+
+Console.WriteLine("Пожалуйста, ввведите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Пожалуйста, ввведите второе число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Пожалуйста, ввведите третье число");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+int max = numberA;
+
+if (max < numberB)
+{
+    max = numberB;
+}
+if (max < numberC)
+{
+    max = numberC;
+}
+Console.Write($"{max} ");
