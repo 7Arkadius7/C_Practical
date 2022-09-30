5e164804f6db2d0c95e881fcf1bd210b5cb1fcde
Sergey <motor-person@yandex.ru>
2022-09-30 14:51:41 +0400

    Сделали задание Task_05

---
+++ Task_05/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("Пожалуйста, ввведите число");
+
+int numberA = Convert.ToInt32(Console.ReadLine());
+int counter = -numberA;
+while (counter <= numberA)
+{
+    Console.Write($"{counter} ");
+    counter++;
+}
