4d14db57ba261ddc58c3947a49b9b3681e037a78
Sergey <motor-person@yandex.ru>
2022-09-30 14:35:08 +0400

    Сделали задание Task_03

---
+++ Task_03/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Пожалуйста, ввведите число от 1 до 7");
+
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+if (numberA == 1) Console.WriteLine("Данному числу соответствует понедельник");
+else if (numberA == 2) Console.WriteLine("Данному числу соответствует вторник");
+else if (numberA == 3) Console.WriteLine("Данному числу соответствует среда");
+else if (numberA == 4) Console.WriteLine("Данному числу соответствует четверг");
+else if (numberA == 5) Console.WriteLine("Данному числу соответствует пятница");
+else if (numberA == 6) Console.WriteLine("Данному числу соответствует суббота");
+else if (numberA == 7) Console.WriteLine("Данному числу соответствует воскресенье");
+else Console.WriteLine("Вы ввели не корректное число");
