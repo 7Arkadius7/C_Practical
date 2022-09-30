9f6a6cbfad2e40878e087d39bae61e06432635dc
Sergey <motor-person@yandex.ru>
2022-09-30 15:24:20 +0400

    Сделали задание Task_02

---
+++ Task_02/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("Пожалуйста, ввведите первое число");
+
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Пожалуйста, ввведите второе число");
+
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > numberB)
+{
+    Console.WriteLine($"Число {numberA} больше {numberB}");
+}
+else
+{
+    Console.WriteLine($"Число {numberB} больше {numberA}");
+}
