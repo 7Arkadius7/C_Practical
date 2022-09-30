d951adb1588b8483c6e740c98002c8e9c0493197
Sergey <motor-person@yandex.ru>
2022-09-30 15:52:46 +0400

    Сделали задание Task_06

---
+++ Task_06/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("Пожалуйста, ввведите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int res = num %  2;
+if (res == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+Console.WriteLine("Нет");
