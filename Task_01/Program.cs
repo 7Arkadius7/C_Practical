979618afd76f8a5d92e1375f7660b3a0bc4396ad
Sergey <motor-person@yandex.ru>
2022-09-30 14:15:34 +0400

    Сделали задачу Task_ 01

---
+++ Task_01/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.WriteLine("Пожалуйста, ввведите первое число");
+
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Пожалуйста, ввведите второе число");
+
+int numberB = Convert.ToInt32(Console.ReadLine());
+int squareB = numberB * numberB;
+
+if (numberA == squareB)
+{
+    Console.WriteLine($"Число {numberA} является квадратом числа {numberB}");
+}
+else 
+{
+    Console.WriteLine($"Число {numberA} НЕ является квадратом числа {numberB}");
+}
